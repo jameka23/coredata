ff452f8df5dbee8af28acb37a65a2d1a647cf070
Jameka Echols <jameka.echols@gmail.com>
2022-05-15 20:14:45 -0500

    Initial Commit

---
+++ CoreDataCRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by Jameka Echols on 5/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
